c94abe496054749fd9cdb82b84b801ea8866310e
Shahriar Hakim <shahriarhakim@192-168-1-192.tpgi.com.au>
2024-07-28 22:13:19 +1000

    patch2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
-// src/components/Home.js
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import paint1 from '../paint1.jpeg';
+import paint2 from '../paint2.jpeg';
 
 const Home = ({ src, alt = '' }) => {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,11 +20,21 @@ const Home = ({ src, alt = '' }) => {
   }, [src]);
 
   return (
-    <div className='flex items-center justify-center h-screen bg-gray-100'>
+    <div className="flex flex-col sm:flex-row items-center justify-center min-h-screen bg-gray-100 p-4 sm:p-6 mt-16">
       <img
         src={src}
         alt={alt}
-        className={`transition-opacity duration-1000 ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+        className={`transition-opacity duration-1000 ${isLoaded ? 'opacity-100' : 'opacity-0'} w-full sm:w-1/2 lg:w-1/3 object-contain`}
+      />
+      <img
+        src={paint1}
+        alt={alt}
+        className={`transition-opacity duration-1000 ${isLoaded ? 'opacity-100' : 'opacity-0'} w-full sm:w-1/2 lg:w-1/3 object-contain`}
+      />
+      <img
+        src={paint2}
+        alt={alt}
+        className={`transition-opacity duration-1000 ${isLoaded ? 'opacity-100' : 'opacity-0'} w-full sm:w-1/2 lg:w-1/3 object-contain`}
       />
     </div>
   );
